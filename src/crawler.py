2cceb9a3d8a09a3e035e9a38f7de893121005dfc
Der-Henning <henning.merklinger@gmail.com>
2021-06-08 17:32:31 +0000

    refractored crawler.py, updated schema

--- src/crawler.py
+++ src/crawler.py
@@ -9,6 +9,9 @@ import datetime
 from solr import Solr
 from bs4 import BeautifulSoup
 from fileCache import FileCache
+from buildSchema import langs
+
+fieldList = "id,md5,source,rating,publishers,document,authors,publicationDate,language,title_txt_*,subtitle_txt_*,tags_txt_*,summary_txt_*,creationDate,modificationDate"
 
 class Crawler:
     def __init__(self):
@@ -62,12 +65,6 @@ class Crawler:
 
     def worker(self, stopped, text, progress, status, startable, autorestart):
         try:
-            # print(config.PRE_CLEANUP)
-            # if config.PRE_CLEANUP:
-            #     print("starting cleaning")
-            #     status.value = "Nicht mehr vorhandene Documente löschen ..."
-            #     self.cleanup(self.Documents())
-
             status.value = "Calibre Datenbank einlesen ..."
             documents = self.Documents()
             status.value = "Dokumente werden indiziert ..."
@@ -119,12 +116,16 @@ class Crawler:
         rows = 100
         while offset < numFound:
             try:
-                res = self.solr.select({"q": "*:*", "fl": "id", "rows": rows, "start": offset})
+                res = self.solr.select({"q": "*:*", "fl": fieldList, "rows": rows, "start": offset})
                 solrDocs = res["response"]["docs"]
                 for solrDoc in solrDocs:
                     if not solrDoc["id"] in self.indexedIDs:
-                        print(f"Deleting {solrDoc['id']} from index")
-                        self.solr.remove(solrDoc["id"])
+                        print(f"mark {solrDoc['id']} as deleted")
+                        doc = solrDoc
+                        doc["deleted"] = True
+                        doc["md5"] = ""
+                        doc["scanDate"] = datetime.datetime.now().isoformat() + "Z"
+                        self.solr.commit(doc)
                         self.fileCache.remove(solrDoc["id"])
                 numFound = res["response"]["numFound"]
             except:
@@ -140,12 +141,15 @@ class Crawler:
                     hash_md5.update(chunk)
             return hash_md5.hexdigest()
 
-        solrDoc = self.solr.select({"q":f"id:{doc['id']}", "fl": "md5"})
+        # get existing doc from solr
+        solrDoc = self.solr.select({"q":f"id:{doc['id']}", "fl": fieldList})
 
+        # read md5 if exists
         md5 = ""
-        if len(solrDoc["response"]["docs"]) > 0:
+        if len(solrDoc["response"]["docs"]) > 0 and "md5" in solrDoc["response"]["docs"][0]:
             md5 = solrDoc["response"]["docs"][0]["md5"]
 
+        # get file
         if "indexlink" in doc:
             filePath = self.fileCache.download(doc["indexlink"], doc["id"])
             doc.pop("indexlink", None)
@@ -155,52 +159,81 @@ class Crawler:
         elif "link" in doc:
             filePath = self.fileCache.download(doc["link"], doc["id"])
             doc['file'] = filePath
-        else: return
+        # else: return
 
+        # if no file provided delete cached file
+        # if exists in solr -> mark as deleted else stop
         if doc['file'] == None or not os.path.exists(doc['file']):
             print("No file found")
-            print(f"Deleting {doc['id']} from index")
-            self.solr.remove(doc["id"])
             self.fileCache.remove(doc["id"])
-            return
-
-        doc["md5"] = tomd5(filePath)
-        print(doc["md5"])
-        if md5 != doc["md5"]:
-            print("new Document")
-            extract = self.extractData(filePath, doc)
-            doc.update(extract['pages'])
-            if not "language" in doc: doc['language'] = extract['language']
-            if not "title" in doc: doc["title"] = extract["title"]
-            if "title" in doc: doc[f"title_txt_{doc['language']}"] = doc["title"]
-            if "tags" in doc: doc[f"tags_txt_{doc['language']}"] = doc["tags"]
-            if "creationDate" in extract: doc["creationDate"] = extract["creationDate"]
-            if "modificationDate" in extract: doc["modificationDate"] = extract["modificationDate"]
-            doc["scanDate"] = datetime.datetime.now().isoformat() + "Z"
-            doc.pop("tags", None)
-            doc.pop("title", None)
-            self.solr.commit(doc)
+            if len(solrDoc["response"]["docs"]) > 0:
+                doc["deleted"] = True
+                doc.update(solrDoc["response"]["docs"][0])
+                doc["md5"] = ""
+            else:
+                return
+        
+        # read file and compare md5
+        # if md5 matches saved one -> stop
+        # else extract data from file
         else:
-            print("no changes")
+            doc["md5"] = tomd5(filePath)
+            print(doc["md5"])
+            if md5 != doc["md5"]:
+                print("new Document")
+                extract = self.extractData(filePath)
+                if not extract:
+                    print("Error extracting data from file")
+                    return
+                if not "title" in doc: doc["title"] = extract["title"]
+                if "language" in extract and not "language" in doc: doc['language'] = extract['language']
+                if "creationDate" in extract: doc["creationDate"] = extract["creationDate"]
+                if "modificationDate" in extract: doc["modificationDate"] = extract["modificationDate"]
+                doc["pages"] = extract['pages']
+                doc["deleted"] = False
+            else:
+                print("no changes")
+                return
 
-    def extractData(self, filePath, doc):
+        # set document language for fields
+        if not "language" in doc: doc["language"] = "other"
+        if not doc['language'] in langs: doc['language'] = "other"
+        if "title" in doc:
+            doc[f"title_txt_{doc['language']}"] = doc["title"]
+            doc.pop("title", None)
+        if "tags" in doc:
+            doc[f"tags_txt_{doc['language']}"] = doc["tags"]
+            doc.pop("tags", None)
+        if "subtitle" in doc:
+            doc[f"subtitle_txt_{doc['language']}"] = doc["subtitle"]
+            doc.pop("subtitle", None)
+        if "summary" in doc:
+            doc[f"summary_txt_{doc['language']}"] = doc["summary"]
+            doc.pop("summary", None)
+        if "pages" in doc:
+            doc.update({f"p_{num}_page_txt_{doc['language']}": page for num, page in enumerate(doc['pages'], start=1)})
+            doc.pop("pages", None)
+        doc["scanDate"] = datetime.datetime.now().isoformat() + "Z"
+
+        # commit document to solr
+        self.solr.commit(doc)
+
+    def extractData(self, filePath):
         def getPages(html):
             soup = BeautifulSoup(html, 'html.parser')
             pages = [s.get_text() for s in soup.find_all("div", "page")]
             return pages
         data = {}
-        extract = self.solr.extract(filePath)
-        filename = os.path.basename(filePath)
-        content = extract["file"] if "file" in extract else extract[filename]
-        meta = extract["file_metadata"] if "file_metadata" in extract else extract[f"{filename}_metadata"]
-        meta = dict(zip(meta[::2], meta[1::2]))
-        data['title'] = meta['dc:title'][0] if 'dc:title' in meta else filename
-        if "language" in doc:
-            data['language'] = doc['language']
-        else:
-            data['language'] = meta['language'][0].lower() if "language" in meta else "de"
-            data['language'] = data['language'] if data['language'] == "de" or data['language'] == "en" else "other"
-        data['pages'] = {f"p_{num}_page_txt_{data['language']}": page for num, page in enumerate(getPages(content), start=1)}
-        if "created" in meta: data['creationDate'] = meta['created'][0] 
-        if "Last-Modified" in meta: data['modificationDate'] = meta['Last-Modified'][0]
+        try:
+            extract = self.solr.extract(filePath)
+            filename = os.path.basename(filePath)
+            content = extract["file"] if "file" in extract else extract[filename]
+            meta = extract["file_metadata"] if "file_metadata" in extract else extract[f"{filename}_metadata"]
+            meta = dict(zip(meta[::2], meta[1::2]))
+            data['title'] = meta['dc:title'][0] if 'dc:title' in meta else filename
+            data['pages'] = getPages(content)
+            if "created" in meta: data['creationDate'] = meta['created'][0] 
+            if "Last-Modified" in meta: data['modificationDate'] = meta['Last-Modified'][0]
+            if "language" in meta: data['language'] = meta['language'][0].lower()
+        except: pass
         return data
